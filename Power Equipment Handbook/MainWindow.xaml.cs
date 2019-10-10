2e424fb6d23833d525d8a2d9d6830e4630e78940
Egor Lyukhanov <ealuhanov@yandex.ru>
2019-10-10 23:35:07 +0500

    Stage 4.2 - 3. GUI Development
    Finish general Lines and Trans GUI structure.
    Start automatizaion process on field reletions.

--- Power Equipment Handbook/MainWindow.xaml.cs
+++ Power Equipment Handbook/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace Power_Equipment_Handbook
         }
 
 
-#region Helpers
+        #region Helpers
 
         /// <summary>
         /// Проверка ввода цифр
@@ -75,9 +75,65 @@ namespace Power_Equipment_Handbook
             }
         }
 
-        private void Tab_Elements_SelectionChanged()
+        /// <summary>
+        /// Расчет параметров Линии по Длине и Удельным парамертрам (автоподстановка)
+        /// </summary>
+        private void Line_Calculations(object sender, TextChangedEventArgs e)
         {
+            DotCommaReplacer(sender: sender, e: e);
+            if (txtLength_L.Text == "") return;
 
+            var use = (TextBox)sender;
+
+            System.Globalization.NumberFormatInfo provider = new System.Globalization.NumberFormatInfo();
+            provider.NumberDecimalSeparator = ".";
+
+            double L = Convert.ToDouble(txtLength_L.Text, provider);
+
+            if (use != txtLength_L)
+            {
+                double res;
+                if (use.Text != "") res = Convert.ToDouble(use.Text, provider) * L;
+                else return;
+
+                if (use == txtr0_L) txtR_L.Text = res.ToString();
+                if (use == txtx0_L) txtX_L.Text = res.ToString();
+                if (use == txtb0_L) txtB_L.Text = res.ToString();
+                if (use == txtg0_L) txtG_L.Text = res.ToString();
+            }
+            else if(use == txtLength_L)
+            {
+                if(txtr0_L.Text != "") txtR_L.Text = (Convert.ToDouble(txtr0_L.Text, provider) * L).ToString();
+                if (txtx0_L.Text != "") txtX_L.Text = (Convert.ToDouble(txtx0_L.Text, provider) * L).ToString();
+                if (txtb0_L.Text != "") txtB_L.Text = (Convert.ToDouble(txtb0_L.Text, provider) * L).ToString();
+                if (txtg0_L.Text != "") txtG_L.Text = (Convert.ToDouble(txtg0_L.Text, provider) * L).ToString();
+            }
+        }
+        
+        #endregion
+
+        #region Обработчики конкретных событий
+        /// <summary>
+        /// Обработка внешнего вида формы при изменении типа трансформатора
+        /// </summary>
+        private void CmbType_T_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbType_T.SelectedValue == cmbType_T.Items[0])
+            {
+                grpMid.Visibility = grpLow.Visibility = Visibility.Hidden;
+                lblKH_KML_T.Content = "Kтр ВН";
+                lblEndHighNode_T.Content = "Конец";
+                elUnom_T_2.Visibility = lblUnomLowDouble_T.Visibility = txtUnomLowDouble_T.Visibility = Visibility.Visible;
+                elUnom_T_1.Width = 6;
+            } 
+            else if (cmbType_T.SelectedValue == cmbType_T.Items[1])
+            {
+                grpMid.Visibility = grpLow.Visibility = Visibility.Visible;
+                lblKH_KML_T.Content = "Kтр В(Ктр С-Н)";
+                lblEndHighNode_T.Content = "Ср. точка";
+                elUnom_T_2.Visibility = lblUnomLowDouble_T.Visibility = txtUnomLowDouble_T.Visibility = Visibility.Hidden;
+                elUnom_T_1.Width = 65;
+            } 
         }
 
         #endregion
