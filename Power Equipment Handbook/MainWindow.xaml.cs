3ec931eb6d17095dfc4579146a71931ce1abb5bb
Егор Люханов <ealuhanov@yandex.ru>
2019-10-11 18:41:47 +0500

    Stage 4.3 - 4. GUI development
    Added DataGrids for result tables. Create another pattern for instances.
    Added automatization on cells and interaction.

--- Power Equipment Handbook/MainWindow.xaml.cs
+++ Power Equipment Handbook/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,16 @@ namespace Power_Equipment_Handbook
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataGridTracker track;
         public MainWindow()
         {
             InitializeComponent();
+            track = new DataGridTracker(grdNodes, grdBranches);
+
+            track.AddNode(new Node(1, 110, "нагр."));
+            track.AddNode(new Node(2, 110, "нагр."));
+            track.AddBranch(new Branch(1, 2, "ЛЭП", npar: 1));
+            track.AddBranch(new Branch(1, 2, "ЛЭП", npar: 2));
         }
 
 
@@ -80,13 +88,14 @@ namespace Power_Equipment_Handbook
         /// </summary>
         private void Line_Calculations(object sender, TextChangedEventArgs e)
         {
-            DotCommaReplacer(sender: sender, e: e);
-            if (txtLength_L.Text == "") return;
+            //TODO обработчики неверного заполнения (цвета)
+            DotCommaReplacer(sender: sender, e: e); //Обработка запятых
+            if (txtLength_L.Text == "") return;     //Проверка поля Длина
 
             var use = (TextBox)sender;
+            if (use.Text.StartsWith(".")) return; 
 
-            System.Globalization.NumberFormatInfo provider = new System.Globalization.NumberFormatInfo();
-            provider.NumberDecimalSeparator = ".";
+            NumberFormatInfo provider = new NumberFormatInfo() { NumberDecimalSeparator = "."};
 
             double L = Convert.ToDouble(txtLength_L.Text, provider);
 
@@ -96,10 +105,8 @@ namespace Power_Equipment_Handbook
                 if (use.Text != "") res = Convert.ToDouble(use.Text, provider) * L;
                 else return;
 
-                if (use == txtr0_L) txtR_L.Text = res.ToString();
-                if (use == txtx0_L) txtX_L.Text = res.ToString();
-                if (use == txtb0_L) txtB_L.Text = res.ToString();
-                if (use == txtg0_L) txtG_L.Text = res.ToString();
+                if (use == txtr0_L) txtR_L.Text = res.ToString(); if (use == txtx0_L) txtX_L.Text = res.ToString();
+                if (use == txtb0_L) txtB_L.Text = res.ToString(); if (use == txtg0_L) txtG_L.Text = res.ToString();
             }
             else if(use == txtLength_L)
             {
@@ -110,9 +117,48 @@ namespace Power_Equipment_Handbook
             }
         }
         
+        /// <summary>
+        /// Расчет коэффициентов трансформации
+        /// </summary>
+        private void Ktr_Calculations(object sender, TextChangedEventArgs e)
+        {
+            //TODO обработчики неверного заполнения(цвета)
+            DotCommaReplacer(sender: sender, e: e); //Обработка запятых
+
+            var HV = txtUnomHigh_T; var LV = txtUnomLowDouble_T;
+            var MHV = txtUnomMid_T; var LHV = txtUnomLow_T;
+
+            if (HV.Text.StartsWith(".")) return;
+            if (cmbType_T.Text == "двух.")
+            {
+                if(!string.IsNullOrEmpty(HV.Text) & !string.IsNullOrEmpty(LV.Text))
+                {
+                    if (LV.Text.StartsWith(".")) return;
+                    txtKH_KML_T.Text = (Convert.ToDouble(LV.Text, CultureInfo.InvariantCulture) / Convert.ToDouble(HV.Text, CultureInfo.InvariantCulture)).ToString();
+                }
+            }
+            if(cmbType_T.Text == "тр./АТ")
+            {
+                txtKH_KML_T.Text = "1";
+
+                if(!string.IsNullOrEmpty(HV.Text) & !string.IsNullOrEmpty(MHV.Text))
+                {
+                    if (MHV.Text.StartsWith(".")) return;
+                    txtKHM_T.Text = (Convert.ToDouble(MHV.Text, CultureInfo.InvariantCulture) / Convert.ToDouble(HV.Text, CultureInfo.InvariantCulture)).ToString();
+                }
+                if (!string.IsNullOrEmpty(HV.Text) & !string.IsNullOrEmpty(LHV.Text))
+                {
+                    if (LHV.Text.StartsWith(".")) return;
+                    txtKHL_T.Text = (Convert.ToDouble(LHV.Text, CultureInfo.InvariantCulture) / Convert.ToDouble(HV.Text, CultureInfo.InvariantCulture)).ToString();
+                }
+            }
+        }
+
         #endregion
 
+
         #region Обработчики конкретных событий
+
         /// <summary>
         /// Обработка внешнего вида формы при изменении типа трансформатора
         /// </summary>
@@ -128,6 +174,7 @@ namespace Power_Equipment_Handbook
             } 
             else if (cmbType_T.SelectedValue == cmbType_T.Items[1])
             {
+                txtKH_KML_T.Text = "1";
                 grpMid.Visibility = grpLow.Visibility = Visibility.Visible;
                 lblKH_KML_T.Content = "Kтр В(Ктр С-Н)";
                 lblEndHighNode_T.Content = "Ср. точка";
@@ -135,10 +182,70 @@ namespace Power_Equipment_Handbook
                 elUnom_T_1.Width = 65;
             } 
         }
+        
+        /// <summary>
+        /// Добавить линию в список ветвей
+        /// </summary>
+        private void BtnAdd_L_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO Сделать проверки (цвета)
+            //TODO Добавка узлов 
+            //TODO Проверка на наличие + смещение по счетчику парал. линий
+            int start = int.Parse(txtStartNode_L.Text);
+            int end = int.Parse(txtEndNode_L.Text);
+            int state = (string.IsNullOrWhiteSpace(txtState_L.Text) || int.Parse(txtState_L.Text) == 0) ? 0 : 1;
+            string type = "ЛЭП";
+            int npar = (string.IsNullOrWhiteSpace(txtNpar_L.Text) || int.Parse(txtNpar_L.Text) == 0) ? 0 : int.Parse(txtNpar_L.Text);
+            string name = txtName_L.Text;
+            double r = (string.IsNullOrWhiteSpace(txtR_L.Text) || double.Parse(txtR_L.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtR_L.Text, CultureInfo.InvariantCulture);
+            double x = (string.IsNullOrWhiteSpace(txtX_L.Text) || double.Parse(txtX_L.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtX_L.Text, CultureInfo.InvariantCulture);
+            double b = (string.IsNullOrWhiteSpace(txtB_L.Text) || double.Parse(txtB_L.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtB_L.Text, CultureInfo.InvariantCulture);
+            double g = (string.IsNullOrWhiteSpace(txtG_L.Text) || double.Parse(txtG_L.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtG_L.Text, CultureInfo.InvariantCulture);
+            double ktr = 1;
+            double idd = (string.IsNullOrWhiteSpace(txtIdd_L.Text) || double.Parse(txtIdd_L.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtIdd_L.Text, CultureInfo.InvariantCulture);
+            int region = (string.IsNullOrWhiteSpace(txtRegion_L.Text) || int.Parse(txtRegion_L.Text) == 0) ? 0 : int.Parse(txtRegion_L.Text);
+
+            track.AddBranch(new Branch(start: start, end: end, type: type, 
+                                       state: state, name: name, npar: npar,
+                                       r: r, x: x, b: b, g: g, 
+                                       ktr: ktr, idd: idd, region: region));
+            Tab_Data.SelectedIndex = 1;
+        }
+
+        private void BtnAdd_T_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO Сделать проверки (цвета)
+            //TODO Добавка узлов 
+            //TODO Проверка на наличие + смещение по счетчику парал. тр-ров
+            if (cmbType_T.Text == "двух.")
+            {
+                int start = int.Parse(txtStartNode_T.Text);
+                int end = int.Parse(txtEndHighNode_T.Text);
+                int state = (string.IsNullOrWhiteSpace(txtState_T.Text) || int.Parse(txtState_T.Text) == 0) ? 0 : 1;
+                string type = "Тр-р";
+                int npar = 0;
+                string name = txtName_L.Text;
+                double r = (string.IsNullOrWhiteSpace(txtRH_T.Text) || double.Parse(txtRH_T.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtRH_T.Text, CultureInfo.InvariantCulture);
+                double x = (string.IsNullOrWhiteSpace(txtXH_T.Text) || double.Parse(txtXH_T.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtXH_T.Text, CultureInfo.InvariantCulture);
+                double b = (string.IsNullOrWhiteSpace(txtBH_T.Text) || double.Parse(txtBH_T.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtBH_T.Text, CultureInfo.InvariantCulture);
+                double g = (string.IsNullOrWhiteSpace(txtGH_T.Text) || double.Parse(txtGH_T.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtGH_T.Text, CultureInfo.InvariantCulture);
+                double ktr = (string.IsNullOrWhiteSpace(txtKH_KML_T.Text) || double.Parse(txtKH_KML_T.Text, CultureInfo.InvariantCulture) == 0) ? 0 : double.Parse(txtKH_KML_T.Text, CultureInfo.InvariantCulture);
+                double idd = 0;
+                int region = (string.IsNullOrWhiteSpace(txtRegion_T.Text) || int.Parse(txtRegion_T.Text) == 0) ? 0 : int.Parse(txtRegion_T.Text);
+
+                track.AddBranch(new Branch(start: start, end: end, type: type,
+                                       state: state, name: name, npar: npar,
+                                       r: r, x: x, b: b, g: g,
+                                       ktr: ktr, idd: idd, region: region));
+
+                Tab_Data.SelectedIndex = 1;
+                return;
+            }
+            //TODO Доделать другой тип транса
+            
+        }
 
         #endregion
-
-
 
         //DBProvider db_prv = new DBProvider("test.db");
         //Status_Text.Text = "Состояние подключения:   " + db_prv.Status;
---
+++ Power Equipment Handbook/src/DataGridTracker.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace Power_Equipment_Handbook.src
+{
+    public class DataGridTracker
+    {
+        public DataGrid grdNodes { get; set; }
+        public DataGrid grdBranches { get; set; }
+
+        public ObservableCollection<Node> Nodes = new ObservableCollection<Node>();
+        public ObservableCollection<Branch> Branches = new ObservableCollection<Branch>();
+
+        public DataGridTracker(DataGrid grdNodes, DataGrid grdBranches)
+        {
+            this.grdNodes = grdNodes;
+            this.grdBranches = grdBranches;
+
+            this.grdNodes.ItemsSource = Nodes;
+            this.grdBranches.ItemsSource = Branches;
+        }
+        public void AddNode(Node node)
+        {
+            Application.Current.Dispatcher.BeginInvoke((Action)delegate ()
+                                                {
+                                                    Nodes.Add(node);
+                                                    grdNodes.UpdateLayout();
+                                                });
+        }
+        public void AddBranch(Branch branch)
+        {
+            Application.Current.Dispatcher.BeginInvoke((Action)delegate ()
+                                                {
+                                                    Branches.Add(branch);
+                                                    grdBranches.UpdateLayout();
+                                                });
+        }
+    }
+
+    public class Branch : INotifyPropertyChanged
+    {
+        private int state;
+        private string type;
+        private int start; private int end;
+        private int npar;
+        private string name;
+        private double r; private double x; private double g; private double b;
+        private double ktr;
+        private double idd;
+        private int region;
+
+        #region Properties
+        public int State
+        {
+            get { return state; }
+            set { SetProperty(ref state, value); }
+        }
+        public string Type
+        {
+            get => type;
+            set { SetProperty(ref type, value); }
+        }
+        public int Start
+        {
+            get => start;
+            set { SetProperty(ref start, value); }
+        }
+        public int End
+        {
+            get => end;
+            set { SetProperty(ref end, value); }
+        }
+        public int Npar
+        {
+            get => npar;
+            set { SetProperty(ref npar, value); }
+        }
+        public string Name
+        {
+            get => name;
+            set { SetProperty(ref name, value); }
+        }
+        public double R
+        {
+            get => r;
+            set { SetProperty(ref r, value); }
+        }
+        public double X
+        {
+            get => x;
+            set { SetProperty(ref x, value); }
+        }
+        public double G
+        {
+            get => g;
+            set { SetProperty(ref g, value); }
+        }
+        public double B
+        {
+            get => b;
+            set { SetProperty(ref b, value); }
+        }
+        public double Ktr
+        {
+            get => ktr;
+            set { SetProperty(ref ktr, value); }
+        }
+        public double Idd
+        {
+            get => idd;
+            set { SetProperty(ref idd, value); }
+        }
+        public int Region
+        {
+            get => region;
+            set { SetProperty(ref region, value); }
+        }
+        #endregion
+        
+        public Branch() { }
+
+        public Branch(int start, int end, string type, int state = 0, string name = "", int npar = 0, 
+                      double r = 0, double x = 0, double b = 0, double g = 0, double ktr = 0,
+                      double idd = 0, int region = 0)
+        {
+            State = state;
+            Type = type;
+            Start = start;
+            End = end;
+            Npar = npar;
+            Name = name;
+            R = r; X = x; B = b; G = g;
+            Ktr = ktr;
+            Idd = idd;
+            Region = region;
+        }
+
+        #region INotifyPropertyChanged interface block
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value)) return false;
+            storage = value; OnPropertyChanged(propertyName); return true;
+        }
+        #endregion
+    }
+
+    public class Node:INotifyPropertyChanged
+    {
+        private int state;
+        private string type;
+        private int number;
+        private int unom;
+        private string name;
+        private double p_n; private double q_n; private double p_g; private double q_g;
+        private double vzd;
+        private double q_min; private double q_max;
+        private double b_sh;
+        private int region;
+
+        #region Properties
+        public int State
+        {
+            get { return state; }
+            set { SetProperty(ref state, value); }
+        }
+        public string Type
+        {
+            get => type;
+            set { SetProperty(ref type, value); }
+        }
+        public int Number
+        {
+            get => number;
+            set { SetProperty(ref number, value); }
+        }
+        public int Unom
+        {
+            get => unom;
+            set { SetProperty(ref unom, value); }
+        }
+        public string Name
+        {
+            get => name;
+            set { SetProperty(ref name, value); }
+        }
+        public double P_n
+        {
+            get => p_n;
+            set { SetProperty(ref p_n, value); }
+        }
+        public double Q_n
+        {
+            get => q_n;
+            set { SetProperty(ref q_n, value); }
+        }
+        public double P_g
+        {
+            get => p_g;
+            set { SetProperty(ref p_g, value); }
+        }
+        public double Q_g
+        {
+            get => q_g;
+            set { SetProperty(ref q_g, value); }
+        }
+        public double Vzd
+        {
+            get => vzd;
+            set { SetProperty(ref vzd, value); }
+        }
+        public double Q_min
+        {
+            get => q_min;
+            set { SetProperty(ref q_min, value); }
+        }
+        public double Q_max
+        {
+            get => q_max;
+            set { SetProperty(ref q_max, value); }
+        }
+        public double B_sh
+        {
+            get => b_sh;
+            set { SetProperty(ref b_sh, value); }
+        }
+        public int Region
+        {
+            get => region;
+            set { SetProperty(ref region, value); }
+        }
+        #endregion
+
+        public Node() { }
+        public Node(int number, int unom, string type, int state = 0, string name = "",
+                      double p_n = 0, double q_n = 0, double p_g = 0, double q_g = 0, 
+                      double vzd = 0, double q_min = 0, double q_max = 0, double b_sh = 0, 
+                      int region = 0)
+        {
+            State = state;
+            Type = type;
+            Number = number;
+            Unom = unom;
+            Name = name;
+            P_n = p_n; Q_n = q_n; P_g = p_g; Q_g = q_g;
+            Vzd = vzd; Q_min = q_min; Q_max = q_max; B_sh = b_sh;
+            Region = region;
+        }
+
+        #region INotifyPropertyChanged interface block
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value)) return false;
+            storage = value; OnPropertyChanged(propertyName); return true;
+        }
+        #endregion
+    }
+}
